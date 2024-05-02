733c853daf6e7fc0431b791d03c8740001b6b7ce
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 03:22:34 +0530

    Added sol for problem B

---
+++ Round916/B.cpp
@@ -0,0 +1,99 @@
+/* https://codeforces.com/contest/1914/problem/B */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define yes cout<<'YES'<<endl;
+#define no cout<<'NO'<<endl;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = LLONG_MAX;
+const int MOD = 1e9 + 7;
+#define DEBUG 1
+#if DEBUG
+#define debug(x) cerr << #x << ' = ' << x << endl;
+#else
+#define debug(x)
+#endif
+#define int long long
+template <typename T>
+void print(const T &x) {
+    for (const auto &val : x) {
+        cout << val << ' ';
+    }
+    cout << endl;
+}
+void dijkstra(int src, int V, vector<vector<int>> adj[])
+{
+    vector<int> dist(V,1e9);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0,src});
+    dist[src] = 0;
+    while(! pq.empty())
+    {
+        int d = pq.top().first;
+        int node = pq.top().second;
+        for(auto it: adj[node])
+        {
+            int adjnode = it[0];
+            int newd = it[1];
+            if(newd + d < dist[adjnode])
+            {
+                dist[adjnode] = newd+d;
+                pq.push({d+newd, adjnode});
+            }
+        }
+    }
+    print(dist);
+}
+void solve() {
+    int n,k;
+    cin>>n>>k;
+    vector<int> ans;
+    if(k==0)
+    {
+        for(int i=n;i>=1;i--)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    ans.push_back(1);
+    for(int i=n-k+1;i<=n;i++)
+    {
+        ans.push_back(i);
+    }
+
+    int rem = n-k;
+    if(rem == 0)
+    {
+        print(ans);
+        return;
+    }
+    for(int i=rem;i>1;i--)
+    {
+        ans.push_back(i);
+    }
+    print(ans);
+
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
