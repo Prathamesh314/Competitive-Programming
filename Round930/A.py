b585317020de3b4a38e8631ab91461024971d8cf
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 02:13:32 +0530

    Added sol for problem A

---
+++ Round930/A.py
@@ -0,0 +1,26 @@
+# problem link :- [https://codeforces.com/contest/1937/problem/A]
+
+def takein(a):
+
+    a = int(input())
+
+
+def takeinarr(a):
+
+    a = list(map(int,input().split()))
+import math
+
+def solve():
+    a = int(input())
+    k = int(math.log2(a))
+    print(int(math.pow(2, k)))
+
+
+t = 1
+
+t = int(input())
+
+for _ in range(t):
+
+    solve()
+ 
