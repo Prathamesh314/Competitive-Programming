522368417e8bb07f39cd1e86de2773c08c8d8285
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 03:43:09 +0530

    Added sol for problem A

---
+++ EducationalRound161/A.cpp
@@ -0,0 +1,97 @@
+/* https://codeforces.com/contest/1922/problem/A */
+
+
+// ----------------- असली माल void solve() में है | -----------------
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define yes cout<<'YES'<<endl;
+#define no cout<<'NO'<<endl;
+#define fr(it,a) for(auto &it:a)
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = LLONG_MAX;
+const int MOD = 1e9 + 7;
+#define DEBUG 1
+#if DEBUG
+#define debug(x) cerr << #x << ' = ' << x << endl;
+#else
+#define debug(x)
+#endif
+#define int long long
+template <typename T>
+void print(const T &x) {
+    for (const auto &val : x) {
+        cout << val << ' ';
+    }
+    cout << endl;
+}
+void dijkstra(int src, int V, vector<vector<int>> adj[])
+{
+    vector<int> dist(V,1e9);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0,src});
+    dist[src] = 0;
+    while(! pq.empty())
+    {
+        int d = pq.top().first;
+        int node = pq.top().second;
+        for(auto it: adj[node])
+        {
+            int adjnode = it[0];
+            int newd = it[1];
+            if(newd + d < dist[adjnode])
+            {
+                dist[adjnode] = newd+d;
+                pq.push({d+newd, adjnode});
+            }
+        }
+    }
+    print(dist);
+}
+// ----------------- रहा नहीं जाता तड़प ही ऐसी है | -----------------
+bool isupper(char ch)
+{
+    return (ch>=65 and ch<=90);
+}
+
+char tolower(char ch)
+{
+    if(ch>=97 and ch<=122) return ch;
+    return char(ch-'A'+32);
+}
+
+void solve() {
+    int n;
+    cin>>n;
+    string a,b,c;
+    cin>>a>>b>>c;
+    set<char> st;
+
+    int f=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i] == c[i] or b[i] == c[i]) f++;
+    }
+    cout<<(f==n?"NO":"YES")<<endl;
+
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
