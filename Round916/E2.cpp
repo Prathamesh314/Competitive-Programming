579195aea4e6bc717dd8a06ddbfdefa84caed5c4
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 03:24:28 +0530

    Added sol for problem E2

---
+++ Round916/E2.cpp
@@ -0,0 +1,90 @@
+/* https://codeforces.com/contest/1914/problem/E2 */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define yes cout<<'YES'<<endl;
+#define no cout<<'NO'<<endl;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = LLONG_MAX;
+const int MOD = 1e9 + 7;
+#define DEBUG 1
+#if DEBUG
+#define debug(x) cerr << #x << ' = ' << x << endl;
+#else
+#define debug(x)
+#endif
+#define int long long
+template <typename T>
+void print(const T &x) {
+    for (const auto &val : x) {
+        cout << val << ' ';
+    }
+    cout << endl;
+}
+void dijkstra(int src, int V, vector<vector<int>> adj[])
+{
+    vector<int> dist(V,1e9);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0,src});
+    dist[src] = 0;
+    while(! pq.empty())
+    {
+        int d = pq.top().first;
+        int node = pq.top().second;
+        for(auto it: adj[node])
+        {
+            int adjnode = it[0];
+            int newd = it[1];
+            if(newd + d < dist[adjnode])
+            {
+                dist[adjnode] = newd+d;
+                pq.push({d+newd, adjnode});
+            }
+        }
+    }
+    print(dist);
+}
+void solve() {
+    
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], b[n];
+        for(auto& it: a) cin >> it;
+        for(auto& it: b) cin >> it;
+        vector<pair<int, int>> v(n);
+        for(int i = 0; i < n; i++) v[i] = {a[i] + b[i], i};
+        sort(v.begin(), v.end());
+        long long ans = 0, k = 0;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if (k % 2 == 0) {
+                ans += a[v[i].second] - 1;
+            }
+            else {
+                ans -= b[v[i].second] - 1;
+            }
+            k++;
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
