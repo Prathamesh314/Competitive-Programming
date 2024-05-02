f32c356f5483198c8a43e2ffd1fe52b5d69281a5
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 03:04:40 +0530

    Added sol for problem A

---
+++ Round908/A.cpp
@@ -0,0 +1,25 @@
+// problem link :- https://codeforces.com/contest/1894/problem/A
+
+
+#include<bits/stdc++.h>
+#define fr(i,start,end) for(int i=(start);i<(end);i++)
+#define vi vector<int>
+#define vll vector<long long>
+#define int long long
+#define newline cout<<endl
+#define pb push_back()
+int i,j;
+using namespace std;
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        cout<<s.back()<<endl;
+    }
+}
