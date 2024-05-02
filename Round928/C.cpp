bc7e98b5bae1b5fe2e501646b8cbc7f816a9af52
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 03:55:13 +0530

    Added sol for problem C

--- Round928/C.cpp
+++ Round928/C.cpp
@@ -1,1 +1,102 @@
-# /* https://codeforces.com/contest/1926/problem/C */
+ /* https://codeforces.com/contest/1926/problem/C */
+
+// ----------------- असली माल void solve() में है | -----------------
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define yes cout<<'YES'<<endl;
+#define no cout<<'NO'<<endl;
+#define fr(it,a) for(auto &it:a)
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = LLONG_MAX;
+const int MOD = 1e9 + 7;
+#define DEBUG 1
+#if DEBUG
+#define debug(x) cerr << #x << ' = ' << x << endl;
+#else
+#define debug(x)
+#endif
+#define int long long
+template <typename T>
+void print(const T &x) {
+    for (const auto &val : x) {
+        cout << val << ' ';
+    }
+    cout << endl;
+}
+void dijkstra(int src, int V, vector<vector<int>> adj[])
+{
+    vector<int> dist(V,1e9);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0,src});
+    dist[src] = 0;
+    while(! pq.empty())
+    {
+        int d = pq.top().first;
+        int node = pq.top().second;
+        for(auto it: adj[node])
+        {
+            int adjnode = it[0];
+            int newd = it[1];
+            if(newd + d < dist[adjnode])
+            {
+                dist[adjnode] = newd+d;
+                pq.push({d+newd, adjnode});
+            }
+        }
+    }
+    print(dist);
+}
+// ----------------- रहा नहीं जाता तड़प ही ऐसी है | -----------------
+int sum1(int n, int a[]) 
+{ 
+    if (n < 10) 
+        return (n * (n + 1) / 2); 
+     
+    int d = (int)(log10(n)); 
+    int p = (int)(ceil(pow(10, d))); 
+    int msd = n / p; 
+     
+    return (msd * a[d] + (msd * (msd - 1) / 2) * p + 
+            msd * (1 + n % p) + 
+            sum1(n % p, a)); 
+} 
+
+int sum2(int n) 
+{ 
+    int d = (int)(log10(n)); 
+    int a[d + 1]; 
+    a[0] = 0; a[1] = 45;
+     
+    for(int i = 2; i <= d; i++) 
+        a[i] = a[i - 1] * 10 + 45 * 
+               (int)(ceil(pow(10, i - 1))); 
+ 
+    return sum1(n, a); 
+}
+void solve() {
+    int n;
+    cin>>n;
+    cout<<(sum2(n))<<endl;
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
+
