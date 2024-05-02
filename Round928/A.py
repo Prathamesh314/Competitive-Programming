8e5c5c480d6ec03a547d1693434aa8b39baf4e81
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 03:54:38 +0530

    ...

--- Round928/A.py
+++ Round928/A.py
@@ -1,11 +1,11 @@
-/* https://codeforces.com/contest/1926/problem/A */
+# /* https://codeforces.com/contest/1926/problem/A */
 
 for _ in range(int(input())):
     s = input()
     counta, countb = 0, 0
     for i in s:
-        if i == 'A':
-            counta+=1
+        if i == "A":
+            counta += 1
         else:
             countb += 1
     if counta > countb:
--- Round928/B.py
+++ Round928/B.py
@@ -1,12 +1,12 @@
-/* https://codeforces.com/contest/1926/problem/B */
+# /* https://codeforces.com/contest/1926/problem/B */
 
 for _ in range(int(input())):
     n = int(input())
     arr = []
     for i in range(n):
         s = input()
-        if '1' in s:
-            arr.append(s.count('1'))
+        if "1" in s:
+            arr.append(s.count("1"))
     arr = set(arr)
     if len(arr) == 1:
         print("SQUARE")
