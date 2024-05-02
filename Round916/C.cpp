edcd3f4c082e44966e146f23eb78d6fabc85cdc2
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 03:23:04 +0530

    Added sol for problem C

---
+++ Round916/C.cpp
@@ -0,0 +1,98 @@
+/* https://codeforces.com/contest/1914/problem/C */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define yes cout<<'YES'<<endl;
+#define no cout<<'NO'<<endl;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+const int INF = LLONG_MAX;
+const int MOD = 1e9 + 7;
+#define DEBUG 1
+#if DEBUG
+#define debug(x) cerr << #x << ' = ' << x << endl;
+#else
+#define debug(x)
+#endif
+#define int long long
+template <typename T>
+void print(const T &x) {
+    for (const auto &val : x) {
+        cout << val << ' ';
+    }
+    cout << endl;
+}
+void dijkstra(int src, int V, vector<vector<int>> adj[])
+{
+    vector<int> dist(V,1e9);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0,src});
+    dist[src] = 0;
+    while(! pq.empty())
+    {
+        int d = pq.top().first;
+        int node = pq.top().second;
+        for(auto it: adj[node])
+        {
+            int adjnode = it[0];
+            int newd = it[1];
+            if(newd + d < dist[adjnode])
+            {
+                dist[adjnode] = newd+d;
+                pq.push({d+newd, adjnode});
+            }
+        }
+    }
+    print(dist);
+}
+void solve() {
+    int n,k;
+    cin>>n>>k;
+    vi a(n,0);
+    vi b(n,0);
+    for(auto &it: a)
+    {
+        cin>>it;
+    }
+    for(auto &it: b)
+    {
+        cin>>it;
+    }
+
+    int ans = 0;
+    int fans = 0;
+    int bmax = 0;
+
+    for (int j = 0; j < n; ++j) {
+        if (k == j) {
+            break;
+        }
+
+        ans += a[j];
+        bmax = max(bmax, b[j]);
+        fans = max(ans + (k - j - 1) * bmax, fans);
+    }
+
+    cout << fans << endl;
+
+}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
