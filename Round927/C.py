437d485a3f286737b334f6fb869b925fc1edd38c
Prathamesh314 <kurvep18@gmail.com>
2024-05-03 03:49:19 +0530

    Added sol for problem C

---
+++ Round927/C.py
@@ -0,0 +1,39 @@
+# /* https://codeforces.com/contest/1932/problem/C */
+
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    s = input()
+
+    l = 0
+    r = n - 1
+    prod = 1
+    ans = []
+
+    for i in range(n - 1):
+        if s[i] == "L":
+            l += 1
+        else:
+            r -= 1
+
+    current = a[r]
+
+    for i in range(n - 2, -2, -1):
+        prod = current % m
+        current = current % m
+        ans.append(prod)
+        if i >= 0:
+            if s[i] == "L":
+                l -= 1
+                current *= a[l]
+            else:
+                r += 1
+                current *= a[r]
+
+    ans.reverse()
+
+    for it in ans:
+        print(it, end=" ")
+
+    print()
